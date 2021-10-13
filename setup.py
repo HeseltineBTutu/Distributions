a1cfb0b5c56bf2d2d89548f1bbe67ea545d7b54e
Heseltine Tutu <heseltinetutu@gmail.com>
2021-10-13 16:35:05 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      author='Heseltine Tutu',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
